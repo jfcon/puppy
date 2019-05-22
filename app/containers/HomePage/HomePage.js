c83c2c934e873b5b9e03dde8dcc6cf052eee0180
John <connollyjohnf@gmail.com>
2019-05-22 23:21:57 +0000

    installed Giphy SDK through npm

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -8,6 +8,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import './style.scss';
+import App from '../App/App';
 
 export default class HomePage extends React.PureComponent {
   // eslint-disable-line react/prefer-stateless-function
@@ -15,10 +16,11 @@ export default class HomePage extends React.PureComponent {
    * when initial state username is not null, submit the form to load repos
    */
   componentDidMount() {
-    const { username, onSubmitForm } = this.props;
-    if (username && username.trim().length > 0) {
-      onSubmitForm();
-    }
+    App.getPuppyGif();
+  }
+
+  getPuppyGif = () => {
+    App.getPuppyGif();
   }
 
   render() {
@@ -35,7 +37,7 @@ export default class HomePage extends React.PureComponent {
           </section>
           <section className="gifSpot">
             {/* <h2>Gimme!</h2> */}
-            <input type="submit" value="Puppy, please!" />
+            <input type="submit" value="Puppy, Please!" onClick={this.getPuppyGif} />
           </section>
         </div>
       </article>
@@ -44,9 +46,6 @@ export default class HomePage extends React.PureComponent {
 }
 
 HomePage.propTypes = {
-  loading: PropTypes.bool,
-  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-  repos: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
   onSubmitForm: PropTypes.func,
   username: PropTypes.string,
   onChangeUsername: PropTypes.func
