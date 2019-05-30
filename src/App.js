34c4c6b17820ebbf1392223faa530866f27412cf
John <connollyjohnf@gmail.com>
2019-05-30 18:53:49 +0000

    turned original function into a react component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+
+  componentDidMount () {
+    const GphApiClient = require('giphy-js-sdk-core')
+    const client = GphApiClient("CyIdybqkZ7SkYoXz8NBOsqIV4bSpDx86")
+    /// Gif Search
+    client.search('gifs', {"q": "puppies"})
+    .then((response) => {
+      response.data.forEach((gifObject) => {
+        console.log(gifObject)
+      })
+    })
+    .catch((err) => {
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
